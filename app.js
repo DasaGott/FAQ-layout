0b6e1472fd8d96659bbeff374fe77f08332026e5
DasaGott <142871686+DasaGott@users.noreply.github.com>
2023-11-08 17:37:01 +0100

    Fixed bug
    questions didnt colapse when other clicked - fixed

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const questions =
   document.querySelectorAll(".question");
+
 questions.forEach(function (question) {
   question.addEventListener("click", toggleClass);
 
   function toggleClass() {
     const button = question.firstElementChild;
     const answer = question.nextElementSibling;
-
-    button.classList.toggle("active");
-    question.classList.toggle("active");
-    answer.classList.toggle("active");
+    if (question.classList.contains("active")) {
+      button.classList.remove("active");
+      question.classList.remove("active");
+      answer.classList.remove("active");
+    } else {
+      removeClass();
+      button.classList.add("active");
+      question.classList.add("active");
+      answer.classList.add("active");
+    }
+  }
+  function removeClass() {
+    questions.forEach((question) => {
+      const button = question.firstElementChild;
+      const answer = question.nextElementSibling;
+      button.classList.remove("active");
+      question.classList.remove("active");
+      answer.classList.remove("active");
+    });
   }
 });
